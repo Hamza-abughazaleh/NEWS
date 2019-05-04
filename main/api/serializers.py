2af6239da1552a6f66559cf75595d71d6b88c158
hamza <hghazaleh@aswaq.com>
2019-05-04 22:27:16 +0300

    edit api

--- main/api/serializers.py
+++ main/api/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
 
-from django.contrib.auth import authenticate
-from django.utils.translation import gettext as _
-
 from main.models import WebsiteInfo, News, NewsWebsite
-from user.models import User
-from user.utils import normalise_email
-
-
-def field_length(fieldname):
-    field = next(
-        field for field in User._meta.fields if field.name == fieldname)
-    return field.max_length
 
 
 class WebSiteInfoSerializer(serializers.HyperlinkedModelSerializer):
@@ -41,46 +30,3 @@ class NewsDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = ('pk', 'title', 'description', 'image', 'url', 'news_website_details')
-
-
-class UserSerializer(serializers.ModelSerializer):
-    user_detail = serializers.HyperlinkedIdentityField(view_name="user-detail", lookup_field='pk')
-    phone_number = serializers.RegexField(required=False, regex=r'^\d{9,15}$' ,max_length=15, help_text=_("In case we need to contact you"),)
-
-    class Meta:
-        model = User
-        fields = ('pk', 'email', 'user_detail','phone_number')
-        read_only_fields = ('phone_number','email')
-
-
-class UserDetailsSerializer(serializers.ModelSerializer):
-    phone_number = serializers.RegexField(required=False, regex=r'^\d{9,15}$', max_length=15, help_text=_("In case we need to contact you"),)
-
-    class Meta:
-        model = User
-        fields = ('email','first_name', 'last_name', 'gender', 'phone_number', 'address')
-        read_only_fields = ('phone_number','email')
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(
-        max_length=field_length(User.USERNAME_FIELD), required=True)
-    password = serializers.CharField(
-        max_length=field_length('password'), required=True)
-
-    def validate(self, attrs):
-        try:
-            normalise_email(attrs['username'])
-        except:
-            raise serializers.ValidationError(_('invalid username or password'))
-        user = authenticate(
-            username=attrs['username'], password=attrs['password'])
-        if user is None:
-            raise serializers.ValidationError(_('invalid login'))
-        elif not user.is_active:
-            raise serializers.ValidationError(
-                _('Can not log in as inactive user'))
-
-        # set instance to the user so we can use this in the view
-        self.instance = user
-        return attrs
